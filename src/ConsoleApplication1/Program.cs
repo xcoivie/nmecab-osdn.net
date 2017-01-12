1f2b7706102fed4237c0cedc12eeb9da11954c96
xuannt.ext <xuannt.ext@mytrax.co.jp>
2017-01-12 10:54:05 +0700

    first commit of original source code of NMeCab 0.07 (https://osdn.net/projects/nmecab/)

---
+++ src/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using NMeCab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeCabTagger tagger = MeCabTagger.Create();
+            string searchString = @"もろみ純米吟醸生酒  もろみ純米吟醸生酒 Shinomine しのみね";
+            //@"大吟醸";
+            string result = tagger.Parse(searchString);
+
+            Console.Write(string.Format("{0} ==> {1}", searchString, result));
+        }
+    }
+}
